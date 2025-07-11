d7d870964a7d9137f3cca162d6a83a5efdedd454
Antonino Ragusa <antonino.ragusa.er@gmail.com>
2025-07-11 12:40:19 +0200

    Update bot.py

--- src/bot.py
+++ src/bot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import commands
+from config import TOKEN
+import asyncio
 import os
-
-TOKEN = os.getenv("DISCORD_TOKEN")
 
 intents = discord.Intents.default()
 bot = commands.Bot(command_prefix="!", intents=intents)
@@ -10,13 +10,19 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 @bot.event
 async def on_ready():
     try:
-        await bot.tree.sync()  # Sync globale senza guild specifico
+        await bot.tree.sync()
         print(f"{bot.user} è online e comandi sincronizzati globalmente.")
     except Exception as e:
         print(f"Errore nella sincronizzazione dei comandi: {e}")
 
-@bot.tree.command(name="ping", description="Risponde con Pong!")
-async def ping(interaction: discord.Interaction):
-    await interaction.response.send_message("🏓 Pong!")
+async def load_extensions():
+    for filename in os.listdir("./src/cogs"):
+        if filename.endswith(".py"):
+            await bot.load_extension(f"cogs.{filename[:-3]}")
 
-bot.run(TOKEN)
+async def main():
+    await load_extensions()
+    await bot.start(TOKEN)
+
+if __name__ == "__main__":
+    asyncio.run(main())
